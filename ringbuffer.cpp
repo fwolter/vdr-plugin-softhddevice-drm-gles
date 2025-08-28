29c068940750fb751c24595c27b2852d7a38f2b0
Andreas Baierl <ichgeh@imkreisrum.de>
2025-08-28 12:03:36 +0200

    refactoring: cDeviceRingBuffer - builds

--- ringbuffer.cpp
+++ ringbuffer.cpp
@@ -1,29 +1,22 @@
-///
-///	@file ringbuffer.cpp	@brief Ringbuffer module
-///
-///	Copyright (c) 2009, 2011, 2014	by Johns.  All Rights Reserved.
-///
-///	Contributor(s):
-///
-///	License: AGPLv3
-///
-///	This program is free software: you can redistribute it and/or modify
-///	it under the terms of the GNU Affero General Public License as
-///	published by the Free Software Foundation, either version 3 of the
-///	License.
-///
-///	This program is distributed in the hope that it will be useful,
-///	but WITHOUT ANY WARRANTY; without even the implied warranty of
-///	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-///	GNU Affero General Public License for more details.
-///
-//////////////////////////////////////////////////////////////////////////////
-
-///
-///	@defgroup Ringbuffer The ring buffer module.
-///
-///	Lock free ring buffer with only one writer and one reader.
-///
+/**
+ * @file ringbuffer.cpp
+ * @brief Ringbuffer module
+ *
+ * Copyright: (c) 2009, 2011, 2014 by Johns. All Rights Reserved.
+ * Copyright: (c) 2025 by Andreas Baierl. All Rights Reserved.
+ *
+ * License: AGPLv3
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -35,276 +28,275 @@
 #include "logger.h"
 
 /******************************************************************************
-**	cDeviceRingbuffer class
-******************************************************************************/
+ * cDeviceRingbuffer class
+ *
+ * Lock free ring buffer with only one writer and one reader
+ *****************************************************************************/
 
 /**
-**	cDeviceRingbuffer constructor
-**
-**	Allocate a new ring buffer
-**
-**	@param size	Size of the ring buffer.
-**
-**	@returns	Allocated ring buffer, must be freed with
-**			RingBufferDel(), NULL for out of memory.
-*/
+ * @brief cDeviceRingbuffer constructor
+ *
+ * Init a new ring buffer
+ *
+ * @param size  Size of the ring buffer
+ */
 cDeviceRingbuffer::cDeviceRingbuffer(size_t size)
 {
-    if (!(Buffer = (char *)malloc(size)))	// allocate buffer
+	if (!(m_pBuffer = (char *)malloc(size)))	// allocate buffer
 	LOGFATAL("cDeviceRinbuffer::cDeviceRingbuffer: can't allocate memory for ringbuffer");
 
-    Size = size;
-    BufferEnd = Buffer + size;
-    ReadPointer = Buffer;
-    WritePointer = Buffer;
-    atomic_set(&Filled, 0);
+	m_Size = size;
+	m_pBufferEnd = m_pBuffer + size;
+	m_pReadPointer = m_pBuffer;
+	m_pWritePointer = m_pBuffer;
+	atomic_set(&m_filled, 0);
 }
 
 /**
-**	cDeviceRingbuffer destructor
-*/
+ * @brief cDeviceRingbuffer destructor
+ */
 cDeviceRingbuffer::~cDeviceRingbuffer(void)
 {
-    free(Buffer);
+	free(m_pBuffer);
 }
 
 /**
-**	Reset ring buffer pointers.
-*/
+ * @brief Reset ring buffer pointers
+ */
 void cDeviceRingbuffer::Reset(void)
 {
-    ReadPointer = Buffer;
-    WritePointer = Buffer;
-    atomic_set(&Filled, 0);
+	m_pReadPointer = m_pBuffer;
+	m_pWritePointer = m_pBuffer;
+	atomic_set(&m_filled, 0);
 }
 
 /**
-**	Advance write pointer in ring buffer.
-**
-**	@param cnt	Number of bytes to be adavanced.
-**
-**	@returns	Number of bytes that could be advanced in ring buffer.
-*/
+ * @brief Advance write pointer in ring buffer
+ *
+ * @param cnt	Number of bytes to be adavanced
+ *
+ * @returns	    Number of bytes that could be advanced in ring buffer
+ */
 size_t cDeviceRingbuffer::WriteAdvance(size_t cnt)
 {
-    size_t n;
+	size_t n;
 
-    n = Size - atomic_read(&Filled);
-    if (cnt > n) {			// not enough space
-	cnt = n;
-    }
-    //
-    //	Hitting end of buffer?
-    //
-    n = BufferEnd - WritePointer;
-    if (n > cnt) {			// don't cross the end
-	WritePointer += cnt;
-    } else {				// reached or cross the end
-	WritePointer = Buffer;
-	if (n < cnt) {
-	    n = cnt - n;
-	    WritePointer += n;
+	n = m_Size - atomic_read(&m_filled);
+	if (cnt > n) {			// not enough space
+		cnt = n;
 	}
-    }
+	//
+	//	Hitting end of buffer?
+	//
+	n = m_pBufferEnd - m_pWritePointer;
+	if (n > cnt) {			// don't cross the end
+		m_pWritePointer += cnt;
+	} else {				// reached or cross the end
+		m_pWritePointer = m_pBuffer;
+		if (n < cnt) {
+		  n = cnt - n;
+		  m_pWritePointer += n;
+		}
+	}
 
-    //
-    //	Only atomic modification!
-    //
-    atomic_add(cnt, &Filled);
-    return cnt;
+	//
+	//	Only atomic modification!
+	//
+	atomic_add(cnt, &m_filled);
+	return cnt;
 }
 
 /**
-**	Write to a ring buffer.
-**
-**	@param buf	Buffer of @p cnt bytes.
-**	@param cnt	Number of bytes in buffer.
-**
-**	@returns	The number of bytes that could be placed in the ring
-**			buffer.
-*/
+ * @brief Write to a ring buffer
+ *
+ * @param buf	buffer of @p cnt bytes to be written
+ * @param cnt	Number of bytes in buffer
+ *
+ * @returns 	The number of bytes that could be placed in the ring
+ *				buffer
+ */
 size_t cDeviceRingbuffer::Write(const void *buf, size_t cnt)
 {
-    size_t n;
+	size_t n;
 
-    n = Size - atomic_read(&Filled);
-    if (cnt > n) {			// not enough space
-	cnt = n;
-    }
-    //
-    //	Hitting end of buffer?
-    //
-    n = BufferEnd - WritePointer;
-    if (n > cnt) {			// don't cross the end
-	memcpy(WritePointer, buf, cnt);
-	WritePointer += cnt;
-    } else {				// reached or cross the end
-	memcpy(WritePointer, buf, n);
-	WritePointer = Buffer;
-	if (n < cnt) {
-	    buf = (uint8_t *)buf + n;
-	    n = cnt - n;
-	    memcpy(WritePointer, buf, n);
-	    WritePointer += n;
+	n = m_Size - atomic_read(&m_filled);
+	if (cnt > n) {			// not enough space
+		cnt = n;
 	}
-    }
+	//
+	//	Hitting end of buffer?
+	//
+	n = m_pBufferEnd - m_pWritePointer;
+	if (n > cnt) {			// don't cross the end
+		memcpy(m_pWritePointer, buf, cnt);
+		m_pWritePointer += cnt;
+	} else {				// reached or cross the end
+		memcpy(m_pWritePointer, buf, n);
+		m_pWritePointer = m_pBuffer;
+		if (n < cnt) {
+			buf = (uint8_t *)buf + n;
+			n = cnt - n;
+			memcpy(m_pWritePointer, buf, n);
+			m_pWritePointer += n;
+		}
+	}
 
-    //
-    //	Only atomic modification!
-    //
-    atomic_add(cnt, &Filled);
-    return cnt;
+	//
+	//	Only atomic modification!
+	//
+	atomic_add(cnt, &m_filled);
+	return cnt;
 }
 
 /**
-**	Get write pointer and free bytes at this position of ring buffer.
-**
-**	@param[out] wp	Write pointer is placed here
-**
-**	@returns	The number of bytes that could be placed in the ring
-**			buffer at the write pointer.
-*/
+ * @brief Get write pointer and free bytes at this position of ring buffer
+ *
+ * @param[out] wp		Write pointer is placed here
+ *
+ * @returns			The number of bytes that could be placed in the ring
+ *						buffer at the write pointer.
+ */
 size_t cDeviceRingbuffer::GetWritePointer(void **wp)
 {
-    size_t n;
-    size_t cnt;
+	size_t n;
+	size_t cnt;
 
-    //	Total free bytes available in ring buffer
-    cnt = Size - atomic_read(&Filled);
+	//	Total free bytes available in ring buffer
+	cnt = m_Size - atomic_read(&m_filled);
 
-    *wp = WritePointer;
+	*wp = m_pWritePointer;
 
-    //
-    //	Hitting end of buffer?
-    //
-    n = BufferEnd - WritePointer;
-    if (n <= cnt) {			// reached or cross the end
-	return n;
-    }
-    return cnt;
+	//
+	//	Hitting end of buffer?
+	//
+	n = m_pBufferEnd - m_pWritePointer;
+	if (n <= cnt) {			// reached or cross the end
+		return n;
+	}
+	return cnt;
 }
 
 /**
-**	Advance read pointer in ring buffer.
-**
-**	@param cnt	Number of bytes to be advanced.
-**
-**	@returns	Number of bytes that could be advanced in ring buffer.
-*/
+ * @brief Advance read pointer in ring buffer
+ *
+ * @param cnt	Number of bytes to be advanced
+ *
+ * @returns		Number of bytes that could be advanced in ring buffer
+ */
 size_t cDeviceRingbuffer::ReadAdvance(size_t cnt)
 {
-    size_t n;
+	size_t n;
 
-    n = atomic_read(&Filled);
-    if (cnt > n) {			// not enough filled
-	cnt = n;
-    }
-    //
-    //	Hitting end of buffer?
-    //
-    n = BufferEnd - ReadPointer;
-    if (n > cnt) {			// don't cross the end
-	ReadPointer += cnt;
-    } else {				// reached or cross the end
-	ReadPointer = Buffer;
-	if (n < cnt) {
-	    n = cnt - n;
-	    ReadPointer += n;
+	n = atomic_read(&m_filled);
+	if (cnt > n) {			// not enough filled
+		cnt = n;
 	}
-    }
+	//
+	//	Hitting end of buffer?
+	//
+	n = m_pBufferEnd - m_pReadPointer;
+	if (n > cnt) {			// don't cross the end
+		m_pReadPointer += cnt;
+	} else {				// reached or cross the end
+		m_pReadPointer = m_pBuffer;
+		if (n < cnt) {
+			n = cnt - n;
+			m_pReadPointer += n;
+		}
+	}
 
-    //
-    //	Only atomic modification!
-    //
-    atomic_sub(cnt, &Filled);
-    return cnt;
+	//
+	//	Only atomic modification!
+	//
+	atomic_sub(cnt, &m_filled);
+	return cnt;
 }
 
 /**
-**	Read from a ring buffer.
-**
-**	@param buf	Buffer of @p cnt bytes.
-**	@param cnt	Number of bytes to be read.
-**
-**	@returns	Number of bytes that could be read from ring buffer.
-*/
+ * @brief Read from a ring buffer.
+ *
+ * @param buf	buffer of @p cnt bytes to be read
+ * @param cnt	Number of bytes to be read
+ *
+ * @returns		Number of bytes that could be read from ring buffer
+ */
 size_t cDeviceRingbuffer::Read(void *buf, size_t cnt)
 {
-    size_t n;
+	size_t n;
 
-    n = atomic_read(&Filled);
-    if (cnt > n) {			// not enough filled
-	cnt = n;
-    }
-    //
-    //	Hitting end of buffer?
-    //
-    n = BufferEnd - ReadPointer;
-    if (n > cnt) {			// don't cross the end
-	memcpy(buf, ReadPointer, cnt);
-	ReadPointer += cnt;
-    } else {				// reached or cross the end
-	memcpy(buf, ReadPointer, n);
-	ReadPointer = Buffer;
-	if (n < cnt) {
-	    buf = (uint8_t *)buf + n;
-	    n = cnt - n;
-	    memcpy(buf, ReadPointer, n);
-	    ReadPointer += n;
+	n = atomic_read(&m_filled);
+	if (cnt > n) {			// not enough filled
+		cnt = n;
 	}
-    }
+	//
+	//	Hitting end of buffer?
+	//
+	n = m_pBufferEnd - m_pReadPointer;
+	if (n > cnt) {			// don't cross the end
+		memcpy(buf, m_pReadPointer, cnt);
+		m_pReadPointer += cnt;
+	} else {				// reached or cross the end
+		memcpy(buf, m_pReadPointer, n);
+		m_pReadPointer = m_pBuffer;
+		if (n < cnt) {
+			buf = (uint8_t *)buf + n;
+			n = cnt - n;
+			memcpy(buf, m_pReadPointer, n);
+			m_pReadPointer += n;
+		}
+	}
 
-    //
-    //	Only atomic modification!
-    //
-    atomic_sub(cnt, &Filled);
-    return cnt;
+	//
+	//	Only atomic modification!
+	//
+	atomic_sub(cnt, &m_filled);
+	return cnt;
 }
 
 /**
-**	Get read pointer and used bytes at this position of ring buffer.
-**
-**	@param[out] rp	Read pointer is placed here
-**
-**	@returns	The number of bytes that could be read from the ring
-**			buffer at the read pointer.
-*/
+ * @brief Get read pointer and used bytes at this position of ring buffer
+ *
+ * @param[out] rp	Read pointer is placed here
+ *
+ * @returns			The number of bytes that could be read from the ring
+ *					buffer at the read pointer
+ */
 size_t cDeviceRingbuffer::GetReadPointer(const void **rp)
 {
-    size_t n;
-    size_t cnt;
+	size_t n;
+	size_t cnt;
 
-    //	Total used bytes in ring buffer
-    cnt = atomic_read(&Filled);
+	//	Total used bytes in ring buffer
+	cnt = atomic_read(&m_filled);
 
-    *rp = ReadPointer;
+	*rp = m_pReadPointer;
 
-    //
-    //	Hitting end of buffer?
-    //
-    n = BufferEnd - ReadPointer;
-    if (n <= cnt) {			// reached or cross the end
-	return n;
-    }
-    return cnt;
+	//
+	//	Hitting end of buffer?
+	//
+	n = m_pBufferEnd - m_pReadPointer;
+	if (n <= cnt) {			// reached or cross the end
+		return n;
+	}
+	return cnt;
 }
 
 /**
-**	Get free bytes in ring buffer.
-**
-**	@returns	Number of bytes free in buffer.
-*/
+ * @brief Get free bytes in ring buffer
+ *
+ * @returns		Number of bytes free in buffer
+ */
 size_t cDeviceRingbuffer::FreeBytes(void)
 {
-    return Size - atomic_read(&Filled);
+	return m_Size - atomic_read(&m_filled);
 }
 
 /**
-**	Get used bytes in ring buffer.
-**
-**	@returns	Number of bytes used in buffer.
-*/
+ * @brief Get used bytes in ring buffer.
+ *
+ * @returns		Number of bytes used in buffer.
+ */
 size_t cDeviceRingbuffer::UsedBytes(void)
 {
-    return atomic_read(&Filled);
+	return atomic_read(&m_filled);
 }
--- ringbuffer.h
+++ ringbuffer.h
@@ -1,26 +1,22 @@
-///
-///	@file ringbuffer.h	@brief Ringbuffer module header file
-///
-///	Copyright (c) 2009, 2011 by Johns.  All Rights Reserved.
-///
-///	Contributor(s):
-///
-///	License: AGPLv3
-///
-///	This program is free software: you can redistribute it and/or modify
-///	it under the terms of the GNU Affero General Public License as
-///	published by the Free Software Foundation, either version 3 of the
-///	License.
-///
-///	This program is distributed in the hope that it will be useful,
-///	but WITHOUT ANY WARRANTY; without even the implied warranty of
-///	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-///	GNU Affero General Public License for more details.
-///
-//////////////////////////////////////////////////////////////////////////////
-
-/// @addtogroup Ringbuffer
-/// @{
+/**
+ * @file ringbuffer.h
+ * @brief Ringbuffer class declaration
+ *
+ * Copyright: (c) 2009, 2011 by Johns. All Rights Reserved.
+ * Copyright: (c) 2025 by Andreas Baierl. All Rights Reserved.
+ *
+ * License: AGPLv3
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ */
 
 #ifndef __DEVICE_RINGBUFFER_H
 #define __DEVICE_RINGBUFFER_H
@@ -31,47 +27,32 @@
 
 #include "iatomic.h"
 
-//----------------------------------------------------------------------------
-//	Ringbuffer
-//----------------------------------------------------------------------------
-
 /**
-**	cRingDeviceBuffer - RingBuffer class
-*/
+ * @brief cRingDeviceBuffer - RingBuffer class
+ */
 
 class cDeviceRingbuffer
 {
 private:
-    char *Buffer;			///< ring buffer data
-    const char *BufferEnd;		///< end of buffer
-    size_t Size;			///< bytes in buffer (for faster calc)
-    const char *ReadPointer;		///< only used by reader
-    char *WritePointer;			///< only used by writer
+	char *m_pBuffer;				///< ring buffer data
+	const char *m_pBufferEnd;		///< end of buffer
+	size_t m_Size;					///< bytes in buffer (for faster calc)
+	const char *m_pReadPointer;		///< only used by reader
+	char *m_pWritePointer;			///< only used by writer
 
-    /// The only thing modified by both
-    atomic_t Filled;			///< how many of the buffer is used
+	// The only thing modified by both
+	atomic_t m_filled;				///< how many of the buffer is used
 public:
-    cDeviceRingbuffer(size_t);
-    virtual ~cDeviceRingbuffer(void);
-    void Reset(void);
-	///< reset ring buffer pointers
-    size_t Write(const void *, size_t);
-	///< write into ring buffer
-    size_t GetWritePointer(void **);
-	///< get write pointer of ring buffer
-    size_t WriteAdvance(size_t);
-	///< advance write pointer of ring buffer
-    size_t Read(void *, size_t);
-	///< read from ring buffer
-    size_t GetReadPointer(const void **);
-	///< get read pointer of ring buffer
-    size_t ReadAdvance(size_t);
-	///< advance read pointer of ring buffer
-    size_t FreeBytes(void);
-	///< free bytes ring buffer
-    size_t UsedBytes(void);
-	///< used bytes ring buffer
+	cDeviceRingbuffer(size_t);
+	virtual ~cDeviceRingbuffer(void);
+	void Reset(void);
+	size_t Write(const void *, size_t);
+	size_t GetWritePointer(void **);
+	size_t WriteAdvance(size_t);
+	size_t Read(void *, size_t);
+	size_t GetReadPointer(const void **);
+	size_t ReadAdvance(size_t);
+	size_t FreeBytes(void);
+	size_t UsedBytes(void);
 };
 #endif
-
-/// @}
