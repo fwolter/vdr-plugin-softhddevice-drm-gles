6a93bb453198092cee5024d33c7900d8d99b7f8c
Andreas Baierl <ichgeh@imkreisrum.de>
2025-09-17 22:50:49 +0200

    refactoring: iatomic.h

--- iatomic.h
+++ iatomic.h
@@ -19,75 +19,42 @@
 #ifndef __IATOMIC_H
 #define __IATOMIC_H
 
-#define GCC_VERSION (__GNUC__ * 10000 \
-	+ __GNUC_MINOR__ * 100 \
-	+ __GNUC_PATCHLEVEL__)
+typedef volatile int atomic_t;  ///< atomic type, 24 bit useable
 
-//	gcc before 4.7 didn't support atomic builtins,
-//	use alsa atomic functions.
-#if GCC_VERSION < 40700
-
-#include <alsa/iatomic.h>
-
-#else
-
-//////////////////////////////////////////////////////////////////////////////
-//	Defines
-//////////////////////////////////////////////////////////////////////////////
-
-//////////////////////////////////////////////////////////////////////////////
-//	Declares
-//////////////////////////////////////////////////////////////////////////////
-
-///
-///	atomic type, 24 bit useable,
-///
-typedef volatile int atomic_t;
-
-//////////////////////////////////////////////////////////////////////////////
-//	Prototypes
-//////////////////////////////////////////////////////////////////////////////
-
-//////////////////////////////////////////////////////////////////////////////
-//	Inlines
-//////////////////////////////////////////////////////////////////////////////
-
-///
-///	Set atomic value.
-///
+/**
+ * Set atomic value.
+ */
 #define atomic_set(ptr, val) \
     __atomic_store_n(ptr, val, __ATOMIC_SEQ_CST)
 
-///
-///	Read atomic value.
-///
+/**
+ * Read atomic value.
+ */
 #define atomic_read(ptr) \
     __atomic_load_n(ptr, __ATOMIC_SEQ_CST)
 
-///
-///	Increment atomic value.
-///
+/**
+ * Increment atomic value.
+ */
 #define atomic_inc(ptr) \
     __atomic_add_fetch(ptr, 1, __ATOMIC_SEQ_CST)
 
-///
-///	Decrement atomic value.
-///
+/**
+ * Decrement atomic value.
+ */
 #define atomic_dec(ptr) \
     __atomic_sub_fetch(ptr, 1, __ATOMIC_SEQ_CST)
 
-///
-///	Add to atomic value.
-///
+/**
+ * Add to atomic value.
+ */
 #define atomic_add(val, ptr) \
     __atomic_add_fetch(ptr, val, __ATOMIC_SEQ_CST)
 
-///
-///	Subtract from atomic value.
-///
+/**
+ * Subtract from atomic value.
+ */
 #define atomic_sub(val, ptr) \
     __atomic_sub_fetch(ptr, val, __ATOMIC_SEQ_CST)
-
-#endif
 
 #endif
